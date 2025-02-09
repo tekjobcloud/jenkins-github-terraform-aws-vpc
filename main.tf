ff79c3911b7662e84e591475580f4b32f3da032b
sultan raphael <tekjobcloud@gmail.com>
2025-02-09 05:43:26 -0600

    First commit, updated Jenkinsfile main.tf

---
+++ main.tf
@@ -0,0 +1,364 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+
+
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+/* ## use the block below if using app.terraform.io [TFE]
+  cloud {
+    organization = "2025-01-11-sultansaturday"
+
+    workspaces {
+      name = "2025-01-14-tfe-api-workflow"
+    }
+  }
+*/
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+resource "aws_vpc" "app1" {
+  cidr_block = "10.32.0.0/16"
+
+  tags = {
+    Name    = "app1"
+    Service = "application1"
+    Owner   = "Ninnell"
+    Planet  = "Jendawald"
+  }
+}
+
+resource "aws_security_group" "app1-sg01-servers" {
+  name        = "app1-sg01-servers"
+  description = "app1-sg01-servers"
+  vpc_id      = aws_vpc.app1.id
+
+  ingress {
+    description = "MyHomePage"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "MyEvilBox"
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "app1-sg01-servers"
+    Service = "application1"
+    Owner   = "Ozzie"
+    Planet  = "Undawald"
+  }
+
+}
+
+
+#These are   for  public
+
+resource "aws_subnet" "public-us-east-1a" {
+  vpc_id                  = aws_vpc.app1.id
+  cidr_block              = "10.32.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "public-us-east-1a"
+    Service = "application1"
+    Owner   = "Ozzie"
+    Planet  = "Undawald"
+  }
+}
+
+resource "aws_subnet" "public-us-east-1b" {
+  vpc_id                  = aws_vpc.app1.id
+  cidr_block              = "10.32.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "public-us-east-1b"
+    Service = "application1"
+    Owner   = "Ozzie"
+    Planet  = "Undawald"
+  }
+}
+
+
+resource "aws_subnet" "public-us-east-1c" {
+  vpc_id                  = aws_vpc.app1.id
+  cidr_block              = "10.32.3.0/24"
+  availability_zone       = "us-east-1c"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "public-us-east-1c"
+    Service = "application1"
+    Owner   = "Ozzie"
+    Planet  = "Undawald"
+  }
+}
+
+#these are for private
+resource "aws_subnet" "private-us-east-1a" {
+  vpc_id            = aws_vpc.app1.id
+  cidr_block        = "10.32.11.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name    = "private-us-east-1a"
+    Service = "application1"
+    Owner   = "Ozzie"
+    Planet  = "Undawald"
+  }
+}
+
+resource "aws_subnet" "private-us-east-1b" {
+  vpc_id            = aws_vpc.app1.id
+  cidr_block        = "10.32.12.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name    = "private-us-east-1b"
+    Service = "application1"
+    Owner   = "Ozzie"
+    Planet  = "Undawald"
+  }
+}
+
+
+resource "aws_subnet" "private-us-east-1c" {
+  vpc_id            = aws_vpc.app1.id
+  cidr_block        = "10.32.13.0/24"
+  availability_zone = "us-east-1c"
+
+  tags = {
+    Name    = "private-us-east-1c"
+    Service = "application1"
+    Owner   = "Ozzie"
+    Planet  = "Undawald"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.app1.id
+
+  tags = {
+    Name    = "app1_IG"
+    Service = "application1"
+    Owner   = "Ozzie"
+    Planet  = "Undawald"
+  }
+}
+
+
+resource "aws_eip" "nat" {
+  vpc = true
+
+  tags = {
+    Name = "nat"
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public-us-east-1a.id
+
+  tags = {
+    Name = "nat"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.app1.id
+
+  route = [
+    {
+      cidr_block                 = "0.0.0.0/0"
+      nat_gateway_id             = aws_nat_gateway.nat.id
+      carrier_gateway_id         = ""
+      destination_prefix_list_id = ""
+      egress_only_gateway_id     = ""
+      gateway_id                 = ""
+      instance_id                = ""
+      ipv6_cidr_block            = ""
+      local_gateway_id           = ""
+      network_interface_id       = ""
+      transit_gateway_id         = ""
+      vpc_endpoint_id            = ""
+      vpc_peering_connection_id  = ""
+    },
+  ]
+
+  tags = {
+    Name = "private"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.app1.id
+
+  route = [
+    {
+      cidr_block                 = "0.0.0.0/0"
+      gateway_id                 = aws_internet_gateway.igw.id
+      nat_gateway_id             = ""
+      carrier_gateway_id         = ""
+      destination_prefix_list_id = ""
+      egress_only_gateway_id     = ""
+      instance_id                = ""
+      ipv6_cidr_block            = ""
+      local_gateway_id           = ""
+      network_interface_id       = ""
+      transit_gateway_id         = ""
+      vpc_endpoint_id            = ""
+      vpc_peering_connection_id  = ""
+    },
+  ]
+
+  tags = {
+    Name = "public"
+  }
+}
+
+resource "aws_route_table_association" "private-us-east-1a" {
+  subnet_id      = aws_subnet.private-us-east-1a.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private-us-east-1b" {
+  subnet_id      = aws_subnet.private-us-east-1b.id
+  route_table_id = aws_route_table.private.id
+}
+resource "aws_route_table_association" "private-us-east-1c" {
+  subnet_id      = aws_subnet.private-us-east-1c.id
+  route_table_id = aws_route_table.private.id
+}
+
+
+#public
+
+resource "aws_route_table_association" "public-us-east-1a" {
+  subnet_id      = aws_subnet.public-us-east-1a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public-us-east-1b" {
+  subnet_id      = aws_subnet.public-us-east-1b.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public-us-east-1c" {
+  subnet_id      = aws_subnet.public-us-east-1c.id
+  route_table_id = aws_route_table.public.id
+}
+
+
+
+
+
+
+
+
+resource "aws_launch_template" "app1_LT" {
+  name_prefix   = "app1_LT"
+  image_id      = "ami-0e2c8caa4b6378d8c"
+  instance_type = "t2.micro"
+
+  key_name = "MyLinuxBox"
+
+  vpc_security_group_ids = [aws_security_group.app1-sg01-servers.id]
+
+  user_data = base64encode(<<-EOF
+    #!/bin/bash
+    yum update -y
+    yum install -y httpd
+    systemctl start httpd
+    systemctl enable httpd
+
+    # Get the IMDSv2 token
+    TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+
+    # Background the curl requests
+    curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/local-ipv4 &> /tmp/local_ipv4 &
+    curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/placement/availability-zone &> /tmp/az &
+    curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/ &> /tmp/macid &
+    wait
+
+    macid=$(cat /tmp/macid)
+    local_ipv4=$(cat /tmp/local_ipv4)
+    az=$(cat /tmp/az)
+    vpc=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/$macid/vpc-id)
+
+    # Create HTML file
+    cat <<-HTML > /var/www/html/index.html
+    <!doctype html>
+    <html lang="en" class="h-100">
+    <head>
+    <title>Details for EC2 instance</title>
+    </head>
+    <body>
+    <div>
+    <h1>Undawald Clan</h1>
+    <h1>Chains Broken in Ireland</h1>
+    <p><b>Instance Name:</b> $(hostname -f) </p>
+    <p><b>Instance Private Ip Address: </b> $local_ipv4</p>
+    <p><b>Availability Zone: </b> $az</p>
+    <p><b>Virtual Private Cloud (VPC):</b> $vpc</p>
+    </div>
+    </body>
+    </html>
+    HTML
+
+    # Clean up the temp files
+    rm -f /tmp/local_ipv4 /tmp/az /tmp/macid
+  EOF
+  )
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name    = "app1_LT"
+      Service = "application1"
+      Owner   = "Ninnell"
+      Planet  = "Jendawald"
+    }
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
